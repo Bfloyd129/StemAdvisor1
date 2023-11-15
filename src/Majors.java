692110ad398f5492daa28f65d4209525addea8a3
bfloyd129 <flobra95669@student.edmonds.edu>
2023-11-15 11:48:13 -0800

    Added the changes made to the majors class done by Stephanie, removed engr electives that wer causing issues, integrated the "nextQuarter array into new changes"

--- src/Majors.java
+++ src/Majors.java
@@ -1,69 +1,99 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Majors {
     static ArrayList ArraylistMajors = new ArrayList();
 
+
     static {
         ArraylistMajors.add("(0) None");
-        ArraylistMajors.add("(1) MechEngr");
-        ArraylistMajors.add("(2) Physics");
-        ArraylistMajors.add("(3) CS");
-        ArraylistMajors.add("(4) chem");
+        ArraylistMajors.add("(1) Mechanical Engr AS-T");
+        ArraylistMajors.add("(2) Physics AS-T");
+        ArraylistMajors.add("(3) CS AA-DTA");
+        ArraylistMajors.add("(4) CS AS-T2");
+        ArraylistMajors.add("(5) Computer Engr AS-T2");
+        ArraylistMajors.add("(6) Chem AS-T1");
     }
     static ArrayList mathList = new ArrayList();
     static {
         mathList.add("(0) None");
-        mathList.add("(1) precalc1");
-        mathList.add("(2) precalc2");
-        mathList.add("(3) calc2");
-        mathList.add("(4) calc3");
-        mathList.add("(5) calc4");
-        mathList.add("(6) linear algebra");
-        mathList.add("(7) differential equations");
+        mathList.add("(1) Algebra for Precalc");
+        mathList.add("(2) Precalc1");
+        mathList.add("(3) Precalc2");
+        mathList.add("(4) Intro to Statistics");
+        mathList.add("(5) Calc I");
+        mathList.add("(6) Calc II");
+        mathList.add("(7) Calc III");
+        mathList.add("(8) Calc IV");
+        mathList.add("(9) Differential Equations");
+        mathList.add("(10) Linear Algebra");
+
 
     }
+
 
     static ArrayList chem = new ArrayList();
     static{
         chem.add("(0) None");
-        chem.add("(1) intro to chem");
-        chem.add("(2) chem 161");
-        chem.add("(3) chem 162");
-        chem.add("(4) chem 163");
+        chem.add("(1) CHEM& 121");
+        chem.add("(2) CHEM& 139");
+        chem.add("(2) CHEM& 161");
+        chem.add("(3) CHEM& 162");
+        chem.add("(4) CHEM& 163");
+        chem.add("(5) CHEM& 261");
+        chem.add("(6) CHEM& 262");
+        chem.add("(7) CHEM& 263");
+
+
+
 
     }
     static ArrayList english = new ArrayList();
     static {
         english.add("(0) None");
-        english.add("(1) engl99");
-        english.add("(2) engl101");
+        english.add("(1) ENGL& 99");
+        english.add("(2) ENGL& 101");
+        english.add("(3) ENGL& 102");
+        english.add("(4) ENGL& 235");
+
 
     }
     static ArrayList CS = new ArrayList();
     static {
         CS.add("(0) None");
-        CS.add("(1) CS115");
-        CS.add("(2) Language 1");
-        CS.add("(3) Language 2");
-        CS.add("(4) Language 3");
+        CS.add("(1) CS 115");
+        CS.add("(2) CS I C++");
+        CS.add("(3) CS II C++");
+        CS.add("(4) CS III C++");
+        CS.add("(5) CS I Java");
+        CS.add("(6) CS II Java");
+        CS.add("(7) CS III Java");
+
+
+
+
     }
     static ArrayList phys = new ArrayList();
     static {
-        phys.add("(0) phys 221");
-        phys.add("(1) phys 221");
-        phys.add("(2) phys 222");
+        phys.add("(0) PHYS& 221");
+        phys.add("(1) PHYS& 222");
+        phys.add("(2) PHYS& 223");
+
+
+
 
 
 
     }
     static ArrayList engr = new ArrayList();
 
+
     static {
-        engr.add("(0) engr 111");
-        engr.add("(1) Statics engr214 ");
-        engr.add("(2) Dynamics engr 215");
-        engr.add("(3) Mechanics of Materials engr 225");
+
+        engr.add("(1) ENGR& 214-Statics");
+        engr.add("(2) ENGR& 215-Dynamics");
+        engr.add("(3) ENGR& 225-Mechanics of Materials");
     }
 
     static ArrayList nextQuarter = new ArrayList();
@@ -77,38 +107,42 @@ public class Majors {
 
 
 
-        public void engr(){
+
+
+    public void engr(){
         int math = 7+1;
         int chem = 2+1;
         int english = 1+1;
         int phys = 2+1;
         int CS = 1+1;
         int total = math + chem + phys + english + CS;
-     }
-     public void phys(){
-         int math = 7+1;
-         int chem = 2+1;
-         int english = 1+1;
-         int phys = 2+1;
-         int CS = 1+1;
-         int total = math + chem + phys + english + CS;
-     }
-     public void CS(){
-         int math = 7+1;
-         int chem = 2+1;
-         int english = 1+1;
-         int phys = 2+1;
-         int CS = 5+1;
-         int total = math + chem + phys + english + CS;
-     }
-     public void setChem(){
-         int math = 7+1;
-         int chem = 2+1;
-         int english = 1+1;
-         int phys = 2+1;
-         int CS = 1+1;
-         int total = math + chem + phys + english + CS;
-     }
+    }
+    public void phys(){
+        int math = 7+1;
+        int chem = 2+1;
+        int english = 1+1;
+        int phys = 2+1;
+        int CS = 1+1;
+        int total = math + chem + phys + english + CS;
+    }
+    public void CS(){
+        int math = 7+1;
+        int chem = 2+1;
+        int english = 1+1;
+        int phys = 2+1;
+        int CS = 5+1;
+        int total = math + chem + phys + english + CS;
+    }
+    public void setChem(){
+        int math = 7+1;
+        int chem = 2+1;
+        int english = 1+1;
+        int phys = 2+1;
+        int CS = 1+1;
+        int total = math + chem + phys + english + CS;
+    }
+
+
 
 
 }
